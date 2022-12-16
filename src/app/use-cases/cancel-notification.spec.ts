f228e46c69300e5308cbe9d9583497d9ae079510
Omar Colombari <omarucolombari@gmail.com>
2022-12-16 19:31:02 -0300

    test: criado teste unitário para use-case CancelNotification

--- src/app/use-cases/cancel-notification.spec.ts
+++ src/app/use-cases/cancel-notification.spec.ts
@@ -2,9 +2,10 @@ import { Content } from '@app/entities/content';
 import { Notification } from '@app/entities/notification';
 import { InMemoryNotificationsRepository } from '@test/repositories/in-memory-notifications-repository';
 import { CancelNotification } from './cancel-notification';
+import { NotificationNotFound } from './errors/notification-not-found';
 
 describe('Cancel notification', () => {
-  it('should to able to send a notification', async () => {
+  it('should to able cancel a notification', async () => {
     const notificationsRepository = new InMemoryNotificationsRepository();
     const cancelNotification = new CancelNotification(notificationsRepository);
 
@@ -23,5 +24,15 @@ describe('Cancel notification', () => {
     expect(notificationsRepository.notifications[0].canceledAt).toEqual(
       expect.any(Date),
     );
+  });
+  it('should not be able to cancel a non existing notification', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const cancelNotification = new CancelNotification(notificationsRepository);
+
+    expect(() => {
+      return cancelNotification.execute({
+        notificationId: 'fake-notification-id',
+      });
+    }).rejects.toThrow(NotificationNotFound);
   });
 });
